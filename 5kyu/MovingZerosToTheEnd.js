a54f57261ef28556a10c83821428081c3928aa49
Mark Linn <marktlinn@gmail.com>
2022-04-27 09:58:07 +0200

    update solution to MovingZerosToTheEnd

--- 5kyu/MovingZerosToTheEnd.js
+++ 5kyu/MovingZerosToTheEnd.js
@@ -1,14 +1,20 @@
 // Write an algorithm that takes an array and moves all of the zeros to the end, preserving the order of the other elements.
 
-function moveZeros(arr) {
-    const resultArr=arr;
-    for(let i = 0; i<resultArr.length; i++) {
-        if(arr[i]===0) {
-            resultArr.push(0);
-            resultArr.splice(resultArr.indexOf(arr[i]),1);
-        }
-    }
-    return resultArr;
-}
+// function moveZeros(arr) {
+//     const resultArr=arr;
+//     for(let i = 0; i<resultArr.length; i++) {
+//         if(arr[i]===0) {
+//             resultArr.push(0);
+//             resultArr.splice(resultArr.indexOf(arr[i]),1);
+//         }
+//     }
+//     return resultArr;
+// }
 
 // would be better solved using filter() method.
+
+function moveZeros(arr) {
+    return arr.filter(elem=> elem !==0).concat(arr.filter(elem=> elem===0));
+}
+
+console.log(moveZeros([1,0,1,1,2,0,3,0,4]))
