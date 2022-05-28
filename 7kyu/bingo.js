ac1107974c8d47d9ddff70e16c11eb156f40b0f4
Mark Linn <marktlinn@gmail.com>
2022-05-28 20:44:26 +0200

    refactoring of solution

--- 7kyu/bingo.js
+++ 7kyu/bingo.js
@@ -1,13 +1,18 @@
-const bingoNums = [2,9,14,7,15]
+// const bingoNums = [2,9,14,7,15]
+
+// function bingo(array){
+//     counter= 0;
+//     array.forEach((num)=> {
+//         if(array.indexOf(num) === -1){
+//             return
+//         } counter++;
+//     })
+//     return counter ===5? 'WIN': 'LOSE'
+//  }
 
 function bingo(array){
-    counter= 0;
-    bingoNums.forEach((num)=> {
-        if(array.indexOf(num) === -1){
-            return
-        } counter++;
-    })
-    return counter ===5? 'WIN': 'LOSE'
- }
+    const bingoNums = [2,9,14,7,15];
+    return bingoNums.every(e=> array.includes(e))? 'Win': 'Lose'
+}
 
 console.log(bingo([2, 14, 2, 7, 9, 15]))
