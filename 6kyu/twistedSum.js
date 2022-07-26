6162114a55d291b8f6f9041caa4ce7b98670ded2
Mark Linn <marktlinn@gmail.com>
2022-07-26 15:46:28 +0200

    updating codewars that have been done

--- 6kyu/twistedSum.js
+++ 6kyu/twistedSum.js
@@ -1,6 +1,12 @@
 /*Find the sum of the digits of all the numbers from 1 to N (both ends included).
 e.g. if N = 5, we have to find the sum of 1+2+3+4+5 
 */
+// create variable result to store sum of calculations
+// loop through numbers from 0-n; if n is less than 10 add the numbers together., pass value to result.
+//if number goes over 10, split the number into a string and use recude to calculate the sum of its elements as numbers
+//with each loop pass the calculation of the number to result
+//finally return result
+
 
 function twistedSum(n) {
     let result = 0
