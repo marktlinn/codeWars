1a0c4f6e8a82a7bcc25a97799225dd0249fb8af0
Mark Linn <marktlinn@gmail.com>
2022-07-30 16:57:11 +0200

    resaved

--- 6kyu/sortArray.js
+++ 6kyu/sortArray.js
@@ -1,6 +1,11 @@
 // Sort the given array of strings in alphabetical order, case insensitive. For example:
 // ["Hello", "there", "I'm", "fine"]  -->  ["fine", "Hello", "I'm", "there"]
 // ["C", "d", "a", "B"])              -->  ["a", "B", "C", "d"]
+// We need to return the array sorted alphabetically, ignoring the case sensitivity.
+// To do so we're going to run a sort, and within that sort call the localeCompare method on the first element
+// we'll compare a to subsequent elements with the sensitivity set to base to remove case sensitivity
+// return the result.
+
 
 sortme = function( names ){
     return names.sort((a,b)=> {
