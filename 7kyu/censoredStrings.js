90061bb74ee46eca740c274be626e949b7874c2b
Mark Linn <marktlinn@gmail.com>
2022-09-01 14:42:32 +0200

    new codewars completed

---
+++ 7kyu/censoredStrings.js
@@ -0,0 +1,35 @@
+/*
+My PC got infected by a strange virus. It only infects my text files and replaces random letters by *, li*e th*s (like this).
+Fortunately, I discovered that the virus hides my censored letters inside root directory.
+It will be very tedious to recover all these files manually, so your goal is to implement uncensor function that does the hard work automatically.
+Examples
+uncensor("*h*s *s v*ry *tr*ng*", "Tiiesae") ➜ "This is very strange"
+uncensor("A**Z*N*", "MAIG") ➜ "AMAZING"
+uncensor("xyz", "") ➜ "xyz"
+*/
+//PseudoCode/Plan
+// if discovered == '' return infected;
+// else
+// declare count variable at 0
+// declare newStr variable = ''
+// loop through infected, adding the letters to the newStr
+// if letter == *, add discovered at index count and increment count by 1
+
+
+function uncensor(infected, discovered) {
+    if(discovered == '') return infected;
+    let count = 0;
+    let newStr = '';
+    for (let letter of infected){
+        if(letter === '*'){
+            newStr += discovered[count];
+            count++;
+        }
+        else{
+            newStr += letter
+        }
+    }
+    return newStr
+}
+
+console.log(uncensor("*h*s *s v*ry *tr*ng*", "Tiiesae"))
