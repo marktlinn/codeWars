f2a163adec4aa8ca66c7034f4d8aed3d0a707f6d
Mark Linn <marktlinn@gmail.com>
2022-11-07 23:08:24 +0100

    saved and uploaded plan

--- 7kyu/findTheLuckyNumbers.js
+++ 7kyu/findTheLuckyNumbers.js
@@ -7,9 +7,8 @@ ghci> filterLucky [1,2,3,4,5,6,7,68,69,70,15,17]
 [7,70,17]
 Don't worry about bad input, you will always receive a finite list of integers.
 \\\\pseudocode////
-create an empty arr = luckyNums
-loop over each element in the array as a string and check if it includes '7'
-    if the elem includes '7' add it to the arr
+filter loop over each element in the array as a string and check if it includes '7'
+    if the elem includes '7' return it
 return the arr
 */
 
