5e9104c4db74ccaf5cccfc13f4b6bbd9bea34036
Mark Linn <marktlinn@gmail.com>
2022-12-29 10:31:20 +0100

    update, added link to codewars challenge

--- 8kyu/grasshopper_summation.rs
+++ 8kyu/grasshopper_summation.rs
@@ -1,3 +1,14 @@
+/* Summation
+Link:
+https://www.codewars.com/kata/55d24f55d7dd296eb9000030/rust
+Write a program that finds the summation of every number from 1 to num. The number will always be a positive integer greater than 0.
+For example (Input -> Output):
+  2 -> 3 (1 + 2)
+  8 -> 36 (1 + 2 + 3 + 4 + 5 + 6 + 7 + 8)
+*/
+
+
+
 fn summation(n: i32) -> i32 {
   let mut nums: i32 = 0;
   for n in 1..=n{
