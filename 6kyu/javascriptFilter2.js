b8c2532959f62736379c0034f73009ff7eee58c0
Mark Linn <marktlinn@gmail.com>
2023-01-14 09:38:33 +0100

    add: new codewars complete

---
+++ 6kyu/javascriptFilter2.js
@@ -0,0 +1,74 @@
+/*
+Link:
+https://www.codewars.com/kata/5262fa26875ed24a3e000029/train/javascript
+A friend of yours is developing an application for a hotel. You should write a function that returns all names of the people on a given floor. Every floor has 6 rooms, and all rooms are numbered in a consecutive way.
+The function has the following signature:
+function roomMates( rooms, floor ){}
+The argument rooms holds all clients in an array, where the index (starts at 0) corresponds to the room-number and holds the name of the client.
+A room is empty if the name at the corresponding array index is the empty string.
+floor is an integer and denotes the floor whose names need to be returned. Floors are numbered starting at 1.
+Your task is to return the names of all occupants on a given floor.
+Example
+  rooms = [ "Jill", "Jackson", "Jan", "Eve", "", "John", "Jimmy", "Tom", "", "Duke" ]
+  Floor 	Index/Room No.		Name
+  1 	0 	Jill
+  1 	1 	Jackson
+  1 	2 	Jan
+  1 	3 	Eve
+  1 	4 	(empty)
+  1 	5 	John
+  2 	6 	Jimmy
+  2 	7 	Tom
+  2 	8 	(empty)
+  2 	9 	Duke
+
+  function roomMates( rooms, 1 ) should return ["Jill", "Jackson", "Jan", "Eve", "John"], and function roomMates( rooms, 2 ) should return ["Jimmy", "Tom", "Duke"].
+
+\\\\pseudocode////
+filter through the results, 
+  times the floor by six to get the last index to check.
+  subtract 6 from the last index to check to get the first door on the floor to check
+    if array's index is >= start && < last index retrun elem.
+*/
+
+function roomMates(rooms, floor) {
+  return rooms.filter((elem, index) => {
+    const lastDoor = floor * 6;
+    start = lastDoor - 6;
+    if (index >= start && index < lastDoor) {
+      return elem;
+    }
+  });
+}
+
+console.log(
+  roomMates(["foo", "bar", "foobar", "barfoo", "foofoo", "barbar"], 1)
+);
+console.log(
+  roomMates(["foo", "bar", "foobar", "barfoo", "foofoo", "barbar"], 2)
+);
+console.log(
+  roomMates(
+    ["Jill", "Jackson", "Jan", "Eve", "", "John", "Jimmy", "Tom", "", "Duke"],
+    2
+  )
+);
+console.log(
+  roomMates(
+    [
+      "foo",
+      "bar",
+      "foobar",
+      "barfoo",
+      "foofoo",
+      "barbar",
+      "ying",
+      "yang",
+      "yingyang",
+      "yangying",
+      "yangyang",
+      "yingying",
+    ],
+    1
+  )
+);
