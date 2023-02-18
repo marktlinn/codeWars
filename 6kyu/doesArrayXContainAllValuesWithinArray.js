8c9f959c327ededd0208cdcf410b8a3a9b91ba41
Mark Linn <marktlinn@gmail.com>
2023-02-18 15:17:07 +0100

    add: codewars link for most recently completed kata

--- 6kyu/doesArrayXContainAllValuesWithinArray.js
+++ 6kyu/doesArrayXContainAllValuesWithinArray.js
@@ -1,6 +1,6 @@
 /*
 Link:
-
+https://www.codewars.com/kata/5143cc9694a24abcd2000001/train/javascript
 We want to extend the array class so that it provides a contains_all? method. This method will always assume that an array is passed in and will return true if all values in the passed in array are present within the current array.
 
 For example:
