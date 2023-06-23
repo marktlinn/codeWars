03803fb89531cd9fad162a4d25879f57fef69315
Mark Linn <marktlinn@gmail.com>
2023-06-23 19:26:57 +0200

    new codewars: python lists and dictionaries

---
+++ 6kyu/triple_trouble.py
@@ -0,0 +1,42 @@
+"""
+Link:
+https://www.codewars.com/kata/55d5434f269c0c3f1b000058/train/python
+Write a function
+triple_double(num1, num2)
+which takes numbers num1 and num2 and returns 1 if there is a straight triple of a number at any place in num1 and also a straight double of the same number in num2.
+If this isn't the case, return 0
+Examples
+  triple_double(451999277, 41177722899) == 1
+  # num1 has straight triple 999s and num2 has straight double 99s
+
+  triple_double(1222345, 12345) == 0
+  # num1 has straight triple 2s but num2 has only a single 2
+
+  triple_double(12345, 12345) == 0
+
+  triple_double(666789, 12345667) == 1
+"""
+
+
+def triple_double(num1, num2):
+    triple_dict = {}
+    for num in str(num1):
+        if num in triple_dict:
+            triple_dict[num] += 1
+        else:
+            triple_dict[num] = 1
+    triples = [num for num in triple_dict if int(triple_dict[num]) >= 3]
+
+    if not triples:
+        return 0
+
+    for triple in triples:
+        quantity = str(num2).count(triple)
+        if quantity >= 2:
+            return 1
+    return 0
+
+
+print(triple_double(451999277, 41177722899))  # 1
+print(triple_double(1222345, 12345))  # 0
+print(triple_double(12345, 12345))  # 0
