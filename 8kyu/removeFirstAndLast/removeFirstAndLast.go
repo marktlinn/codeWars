8e728ce7b2d82714970c91c040958943bb174bb0
Mark Linn <marktlinn@gmail.com>
2023-07-03 19:37:32 +0200

    fix

--- 8kyu/removeFirstAndLast/removeFirstAndLast.go
+++ 8kyu/removeFirstAndLast/removeFirstAndLast.go
@@ -2,9 +2,8 @@
 It's pretty straightforward. Your goal is to create a function that removes the first and last characters of a string. You're given one parameter, the original string. You don't have to worry with strings with less than two characters.
 */
 
-package RemoveChar
+package removeFirstAndLast
 
 func RemoveChar(word string) string {
-  return word[1:-1]  
+  return word[1:len(word)-1]  
 }
-
