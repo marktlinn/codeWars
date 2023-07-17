83e96863a35c92c07164d1d6604b66b41817120d
Mark Linn <marktlinn@gmail.com>
2023-07-17 18:40:16 +0200

    add: codewars => some bascis in Java

---
+++ 8kyu/schoolPaperwork.java
@@ -0,0 +1,30 @@
+/*
+Link:
+https://www.codewars.com/kata/55f9b48403f6b87a7c0000bd/train/java
+Your classmates asked you to copy some paperwork for them. You know that there are 'n' classmates and the paperwork has 'm' pages.
+
+Your task is to calculate how many blank pages do you need. If n < 0 or m < 0 return 0.
+Example:
+
+n= 5, m=5: 25
+n=-5, m=5:  0
+
+*/
+
+public class Paper {
+  public static int paperWork(int n, int m) {
+    if (n < 0 || m < 0) {
+      return 0;
+    }
+    return n * m;
+  }
+
+  public static void main(String[] args) {
+    int result1 = paperWork(5, 4);
+    System.out.println(String.format("result 1: %d ", result1));
+
+    int result2 = paperWork(3, 7);
+    System.out.println(String.format("Result 2: %d", result2));
+  }
+
+}
