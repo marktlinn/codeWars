55c7e6b9b707c4d1450d1d438c72f8b058d9ba1d
Mark Linn <marktlinn@gmail.com>
2023-08-16 21:54:32 +0200

    add: new codewars => caps in Python

---
+++ 7kyu/drop_caps.py
@@ -0,0 +1,25 @@
+"""
+Link: https://www.codewars.com/kata/559e5b717dd758a3eb00005a/train/python
+DropCaps means that the first letter of the starting word of the paragraph should be in caps and the remaining lowercase, just like you see in the newspaper.
+
+But for a change, let"s do that for each and every word of the given String. Your task is to capitalize every word that has length greater than 2, leaving smaller words as they are.
+
+*should work also on Leading and Trailing Spaces and caps.
+
+"apple"            => "Apple"
+"apple of banana"  => "Apple of Banana"
+"one   space"      => "One   Space"
+"   space WALK   " => "   Space Walk   " 
+Note: you will be provided atleast one word and should take string as input and return string as output.
+
+
+"""
+
+
+def drop_cap(words):
+    def capitalize_word(word):
+        return word.capitalize() if len(word.strip()) > 2 else word
+
+    words_list = words.split(" ")
+    processed_words = [capitalize_word(word) for word in words_list]
+    return " ".join(processed_words)
