9124a6990cdab399c6ad502297ee12c01f58903b
Mark Linn <marktlinn@gmail.com>
2023-09-09 19:07:00 +0200

    add: new codewars => Rust fundamentals

---
+++ 8kyu/rust_fundamentals/convert_bool_to_string.rs
@@ -0,0 +1,10 @@
+/*
+Link:
+Implement a function which convert the given boolean value into its string representation.
+
+Note: Only valid inputs will be given.
+*/
+
+fn boolean_to_string(b: bool) -> String {
+  b.to_string()
+}
